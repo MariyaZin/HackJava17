486fb17057b926889194537debff1d704f2a2de8
Alexandre Charbonneau <acharbonneau16@ubishops.ca>
2017-09-28 14:14:55 -0400

    we're trying to co

--- src/Tank.java
+++ src/Tank.java
@@ -1,4 +1,14 @@
+import java.awt.Point;
 
-public class Tank {
-
+public class Tank extends Vehicule{
+	double gunAngle;
+	double gunMaxRange;
+	
+	
+	public Tank(double speed, double maxRange, double turnRate, Point coords){
+		super.maxSpeed = speed;
+		super.turnRate = turnRate;
+		super.coords = coords;
+	}
+	
 }
